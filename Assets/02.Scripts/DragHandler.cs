7acc6f03f2172624796e57a9e80a46106b47d256
윤동영 <ehddud9608@naver.com>
2025-03-27 20:30:22 +0900

    Feat : 드래그 앤 드롭 로직 구현중
    드래그 앤 드롭 로직 구현중 커밋

--- Assets/02.Scripts/DragHandler.cs
+++ Assets/02.Scripts/DragHandler.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 public class DragHandler : MonoBehaviour
 {
     [Header("참조")]
     [SerializeField] private Canvas canvas;
-    //[SerializeField] private PlaceManager placeManager;
+    [SerializeField] private Placement placement;
 
     [Header("Tank Sprite")]
     [SerializeField] private Sprite bodySprite;
@@ -15,7 +16,72 @@ public class DragHandler : MonoBehaviour
     private bool isDrag = false;
     private bool isPlace = false;
 
+    private PlayerInput inputActions;
 
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        inputActions = new PlayerInput();
+    }
+
+    private void Start()
+    {
+        originalPosition = rectTransform.anchoredPosition; //UI 아이콘의 초기 위치를 저장
+    }
+
+    private void Update()
+    {
+        if (isPlace) return; //아직 배치중이라면 빠져나가도록
+
+        if(isDrag)
+        {
+            Vector2 mouseScreenPos = inputActions.PlayerActions.MousePosition.ReadValue<Vector2>();
+        }
+        
+    }
+    private void OnEnable() 
+    {
+        inputActions.Enable(); //액션 활성화
+
+        inputActions.PlayerActions.Click.started += OnLeftClickStarted;  // Input로직 구독
+        inputActions.PlayerActions.Click.started += OnLeftClickCanceled;
+    }
+    private void OnDisable()
+    {
+        inputActions.PlayerActions.Click.canceled -= OnLeftClickStarted; //구독 해제
+        inputActions.PlayerActions.Click.canceled -= OnLeftClickCanceled;
+
+        inputActions.Disable(); //액션 비활성화
+    }
+    private void OnLeftClickStarted(InputAction.CallbackContext context) //드래그시작시
+    {
+        if (isPlace) return;
+        isDrag = true;
+    }
+    private void OnLeftClickCanceled(InputAction.CallbackContext context) //드래그 종료시
+    {
+        if (!isDrag || isPlace) return; //
+            
+        isDrag = false;
+
+        Vector2 mouseScreenPos = inputActions.PlayerActions.MousePosition.ReadValue<Vector2>();
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0f)
+        ); 
+        worldPos.z = 0f; //z축은 고정
+
+        bool isSuccess = placement.TryPlaceTank(worldPos, bodySprite, turretSprite);
+        if(isSuccess)
+        {
+            isPlace = true;
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            rectTransform.anchoredPosition = originalPosition; //드래그 종료후 오브젝트를 배치하지 못했다면
+                                                               // UI의 최초 위치로 
+        }
+    }
 
 
 
--- Assets/02.Scripts/Placement.cs
+++ Assets/02.Scripts/Placement.cs
@@ -11,7 +11,7 @@ public class Placement : MonoBehaviour
     //    Vector3Int cellPos = roadtileMap.WorldToCell(worldPos); // 주어진 월드좌표가 TileMap의 셀좌표로 변환
     //    Vector3 capturedPos = roadtileMap.CellToWorld(cellPos); // 다시 받은 셀좌표를 통해 다시 월드좌표로 변환
 
-    //    if(!CanPlaceTank(cellPos)) //설치를 하지 못한다면
+    //    if (!CanPlaceTank(cellPos)) //설치 못하는 타일이라면
     //    {
     //        return false;
     //    }
@@ -19,15 +19,17 @@ public class Placement : MonoBehaviour
     //    //탱크를 생성로직과 capturedPos를 이어주고 생성하면 끝.
     //    // 누군가 로직 생성시 삭제
     //    GameObject newTank = Instantiate(tankPrefab, capturedPos, Quaternion.identity); //일단 회전은 0으로 고정
-        
+
     //    //Sprite를 교체하는 로직을 호출
     //    // 누군가 로직 생성시 삭제
+    //    return true ;
     //}
 
     private bool CanPlaceTank(Vector3Int cellPos) //타일맵에서 셀좌표의 위치에 어떤 타일이 깔려있는지를 체크
     {
-        TileBase tile = roadtileMap.GetTile(cellPos); //로드타일맵에 어떤 타일이 있는지 데이터를 가져오고
-        if(tile != null)
+        TileBase road = roadtileMap.GetTile(cellPos); // 로드타일맵에 어떤 타일이 있는지 데이터를 가져오고 
+                                                      // 추가적으로 장애물 설치시에도 가능함 조건을 추가
+        if(road != null)
         {
             return false; //타일이 있다면 도로이기 때문에 설치 불가로 false를 반환
         }
