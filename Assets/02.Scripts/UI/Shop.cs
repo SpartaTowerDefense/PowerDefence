98743bdf2a0b518e9d5cb9e49f944b2e21818128
SimHyungjin <tlagud26@naver.com>
2025-03-28 10:30:37 +0900

    chore : 필드 바뀌어서 전체 주석처리

--- Assets/02.Scripts/UI/Shop.cs
+++ Assets/02.Scripts/UI/Shop.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    [SerializeField] private BodyData[] bodySo;
-    [SerializeField] private Transform slotTransform;
-    [SerializeField] private GameObject slot;
+    //[SerializeField] private BodyData[] bodySo;
+    //[SerializeField] private Transform slotTransform;
+    //[SerializeField] private GameObject slot;
 
-    private UIManager uiManager;
-    public BodyData curData;
+    //private UIManager uiManager;
+    //public BodyData curData;
 
-    private void Start()
-    {
-        uiManager = UIManager.Instance;
-        for (int i = 0; i < bodySo.Length; i++)
-        {
-            Slot obj = Instantiate(slot, slotTransform).GetComponent<Slot>();
-            obj.SetData(bodySo[i]);
-        }
-    }
+    //private void Start()
+    //{
+    //    uiManager = UIManager.Instance;
+    //    for (int i = 0; i < bodySo.Length; i++)
+    //    {
+    //        Slot obj = Instantiate(slot, slotTransform).GetComponent<Slot>();
+    //        obj.SetData(bodySo[i]);
+    //    }
+    //}
 
-    void BuyTurret()
-    {
-        if (!curData) return;
-        if (curData.Price > uiManager.Commander.gold) return;
-        uiManager.Commander.SubtractGold(curData.Price);
-    }
+    //void BuyTurret()
+    //{
+    //    if (!curData) return;
+    //    if (curData.Price > uiManager.Commander.gold) return;
+    //    uiManager.Commander.SubtractGold(curData.Price);
+    //}
 }
