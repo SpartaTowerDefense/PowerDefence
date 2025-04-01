34a066b5499075717ab0010bb4c5b8d34418ea9d
bnm03090805 <bnm03090805@gmail.com>
2025-04-01 15:28:39 +0900

    [fix] 스플래시 범위 수정

--- Assets/02.Scripts/Player/Weapon/PenetrationCannon.cs
+++ Assets/02.Scripts/Player/Weapon/PenetrationCannon.cs
@@ -9,7 +9,7 @@ public class PenetrationCannon : CannonBase
     public PenetrationCannon(Sprite sprite, Transform tip, CannonController controller) : base(sprite, tip, controller)
     {
         clip = resource.LoadResource<AudioClip>($"{FireClip}3");
-        data.Inintionalize(3, 0.5f, true, 4f);
+        data.Inintionalize(3, 2f, true, 4f);
         tp = tip;
     }
 
--- Assets/02.Scripts/Player/Weapon/SplashCannon.cs
+++ Assets/02.Scripts/Player/Weapon/SplashCannon.cs
@@ -10,7 +10,7 @@ public class SplashCannon : CannonBase
     public SplashCannon(Sprite sprite, Transform tip, CannonController controller) : base(sprite, tip, controller)
     {
         clip = resource.LoadResource<AudioClip>($"{FireClip}3");
-        data.Inintionalize(3, 0.5f, false, 4f);
+        data.Inintionalize(3, 2f, false, 4f);
         tp = tip;
     }
 
